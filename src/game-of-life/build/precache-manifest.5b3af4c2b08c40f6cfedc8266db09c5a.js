cf639eca0943211142fd00b5c2c70c0792b78f54
troyAmlee <troyamlee19@gmail.com>
2020-10-16 15:33:11 -0500

    [building]

---
+++ src/game-of-life/build/precache-manifest.5b3af4c2b08c40f6cfedc8266db09c5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d16a90074f51adef945ae36c0a036d6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2789883cd78b2874777f",
+    "url": "/static/css/main.2f1670e3.chunk.css"
+  },
+  {
+    "revision": "28af80ebe1292cae98ca",
+    "url": "/static/js/2.ab1f0eb7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.ab1f0eb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2789883cd78b2874777f",
+    "url": "/static/js/main.b2399118.chunk.js"
+  },
+  {
+    "revision": "85d367d40f4625aef104",
+    "url": "/static/js/runtime-main.1281186e.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
